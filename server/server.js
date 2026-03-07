f94a7c9d2bfc2e10b2c441dd4e25bbbb3b897769
Pai-Pratheeksha <pratheekshapai2006@gmail.com>
2026-03-07 17:41:34 +0530

    Add basic frontend page to test Socket.IO connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 
+/* Create HTTP server */
+const server = http.createServer(app);
+
+/* Attach Socket.IO to server */
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+/* Test route */
 app.get("/", (req, res) => {
   res.send("Realtime chat server running");
 });
 
-app.listen(3000, () => {
+/* WebSocket connection */
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+/* Start server */
+server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
